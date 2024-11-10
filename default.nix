05d5f5347e43c7a69bdc5c0ae85473b9c44d862e
syke-dev <syke-dev@gmail.com>
2024-11-10 01:22:24 +0000

    updates

---
+++ default.nix
@@ -0,0 +1,29 @@
+# https://github.com/BirdeeHub/birdeeSystems/blob/a109d8bfa0bdfbfb205cfb196915d1e8f630c7b2/flake.nix#L75
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }@inputs:
+    let
+      # Linux Architecture
+      # System Options: [ "aarch64-linux" "x86_64-linux" ]
+      system    = "x86_64-linux";
+      hostname  = "nyx";
+      pkgs      = import nixpkgs { inherit system; config.allowUnfree = true; };
+      lib       = nixpkgs.lib;
+    in
+    {
+      abc = "test";
+      # rename hostname to starfield :)
+      nixosConfigurations.nyx = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+          ];
+        };
+
+    };
+}
