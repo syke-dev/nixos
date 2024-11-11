187378f46013404090d419c983ddfc8c3d6a642b
syke-dev <syke-dev@gmail.com>
2024-11-11 08:58:13 +0000

    updatesush

--- hosts/starfield/deepcool.nix
+++ hosts/starfield/deepcool.nix
@@ -1,24 +1,25 @@
 { config, lib, pkgs, fetchFromGitHub, rustPlatform, ... }:
 
-{
-  rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage rec {
+  pname = "deepcool";
+  version = "v0.5.2-alpha";
 
-    src = fetchFromGitHub {
-      owner = "Nortank12";
-      repo = "deepcool-digital-linux";
-      rev = "v0.5.2-alpha";
-      # hash = "sha256-+s5RBC3XSgb8omTbUNLywZnP6jSxZBKSS1BmXOjRF8M=";
-    };
+  src = fetchFromGitHub {
+    owner = "Nortank12";
+    repo = "deepcool-digital-linux";
+    rev = "v0.5.2-alpha";
+    # hash = "sha256-+s5RBC3XSgb8omTbUNLywZnP6jSxZBKSS1BmXOjRF8M=";
+  };
 
-    # cargoHash = "sha256-jtBw4ahSl88L0iuCXxQgZVm1EcboWRJMNtjxLVTtzts=";
-    cargoHash = lib.fakeHash;
+  # cargoHash = "sha256-jtBw4ahSl88L0iuCXxQgZVm1EcboWRJMNtjxLVTtzts=";
+  cargoHash = lib.fakeHash;
 
-    meta = {
-      description = "DeepCool digital display controller";
-      homepage = "https://github.com/Nortank12/deepcool-digital-linux";
-      license = lib.licenses.unlicense;
-      maintainers = [ ];
-    };
+  meta = {
+    description = "DeepCool digital display controller";
+    homepage = "https://github.com/Nortank12/deepcool-digital-linux";
+    license = lib.licenses.unlicense;
+    maintainers = [ ];
+  };
 
-  }
 }
+
